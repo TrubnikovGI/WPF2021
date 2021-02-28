a2ed5d3eb8ee92c5865635bdc344f7056a55827f
Jorik <Trubnikov@unhappy.com>
2021-02-28 15:31:32 +0500

    проверка

--- Balistic/Program.cs
+++ Balistic/Program.cs
@@ -6,7 +6,7 @@ namespace Balistic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Just cheking");
         }
     }
 }
