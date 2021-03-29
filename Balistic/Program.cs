ae6d11201b8bbbbdabd5da7d2aeac8fa8dc9289a
Jorik <Trubnikov@unhappy.com>
2021-03-29 18:26:29 +0500

    zero ver

--- Balistic/Program.cs
+++ Balistic/Program.cs
@@ -6,32 +6,28 @@ namespace Balistic
     {
         static void Main(string[] args)
         {
-            double angle, hight, vol, y = 0;
-            int t = 0;
+            double angle, vol;
             angle = Convert.ToDouble(Console.ReadLine());
-            hight = Convert.ToDouble(Console.ReadLine());
             vol = Convert.ToDouble(Console.ReadLine());
             
-            if(angle > Math.PI/2 || hight < 0 || vol <= 0)
+            if(angle >= 90 || vol <= 0)
             {
                 Console.WriteLine("Ошибка ввода данных, координаты полёта тела будут найденный неверно");
             }
 
             angle = (angle * Math.PI) / 180;
-            do
-            {
-                y = hight + (vol * Math.Sin(angle) * t) - (9.81 * t * t) / 2;
-                t += 1;
-            } while (y > 0);
-            t *= 10;
-            double[] abscisa = new double[t];
-            double[] ordinata = new double[t];
+            double t = (2 * vol * Math.Sin(angle)) / 9.81;
+            int l = (int)Math.Round(t)*10;
+            
+            
+            double[] abscisa = new double[l];
+            double[] ordinata = new double[l];
             double k;
-            for (int i = 0; i < t; i++)
+            for (int i = 0; i < l; i++)
             {
                 k = i;
                 abscisa[i] = vol * Math.Cos(angle) * (k/10);
-                ordinata[i] = hight + (vol * Math.Sin(angle) * (k/10)) - (9.81 * (k/10) * (k/10)) / 2;
+                ordinata[i] = (vol * Math.Sin(angle) * (k/10)) - (9.81 * (k/10) * (k/10)) / 2;
                 if (ordinata[i] < 0)
                 {
                     ordinata[i] = 0;
@@ -39,11 +35,11 @@ namespace Balistic
             }
 
             Console.WriteLine("t:   x   y");
-            for (int i = 0; i < t; i++)
+            for (int i = 0; i < l; i++)
             {
                 Console.WriteLine("{0}:   {1}   {2}", i, abscisa[i], ordinata[i]);
             }
-
+            
         }
     }
 }
