5e986ca0a16fafb09b4c8ff15f52a684cbe7e408
Jorik <Trubnikov@unhappy.com>
2021-04-22 21:53:55 +0500

    Euler method

--- Balistic/Program.cs
+++ Balistic/Program.cs
@@ -5,32 +5,38 @@ namespace Balistic
 {
     class Coordinates
     {
-        private double[] abscisa;
-        private double[] ordinata;
-        private int time;
-        public Coordinates(double ang, double v)
+        double ang, vol, mass;
+
+        public Coordinates(double a, double v, double m)
         {
-            ang = (ang * Math.PI) / 180;
-            double t = (2 * v * Math.Sin(ang)) / 9.81;
-            int l = (int)Math.Round(t) * 10;
+            ang = (Math.PI * a) / 180;
+            vol = v;
+            mass = m;
+        }
+        public void Write_to_Console()
+        {
+            double v_x = vol * Math.Cos(ang);
+            double v_y = vol * Math.Sin(ang);
+            double x = 0, y = 0, t = 0.01;
 
-            time = l;
-            abscisa = new double[l];
-            ordinata = new double[l];
+            Console.WriteLine("{0},   |||   {1}", x, y);
 
-            double k;
-            for (int i = 0; i < l; i++)
+            do
             {
-                k = i;
-                abscisa[i] = v * Math.Cos(ang) * (k / 10);
-                ordinata[i] = (v * Math.Sin(ang) * (k / 10)) - (9.81 * (k / 10) * (k / 10)) / 2;
-                if (ordinata[i] < 0)
-                {
-                    ordinata[i] = 0;
-                }
+                x = x + (0.01 * v_x);
+                y = y + (0.01 * v_y);
+
+                if (y < 0)
+                    y = 0;
+
+                v_x = Vol_x(t, v_x, mass, 0.01);
+                v_y = Vol_y(t, v_x, mass, 0.01);
+
+                Console.WriteLine("{0},   |||   {1}", x, y);
+
+                t += 0.01;
             }
-
-
+            while (y > 0) ;
         }
 
         public void Write_to_File()
@@ -38,62 +44,74 @@ namespace Balistic
             string path = @"..\..\..\Flight.csv";
             using (StreamWriter in_File = new StreamWriter(path))
             {
-
-                in_File.Write("t:");
-                in_File.Write(';');
-                for (int i = 0; i < time; i++)
-                {
-                    in_File.Write(i);
-                    in_File.Write(';');
-                }
-
+                in_File.WriteLine("angle: {0}; speed: {1}; mass: {2}", (ang * 180 / Math.PI), vol, mass);
                 in_File.WriteLine(" ");
+                in_File.WriteLine("t:; x:; y:;");
 
-                in_File.Write("x:");
-                in_File.Write(';');
-                for (int i = 0; i < time; i++)
+                double v_x = vol * Math.Cos(ang);
+                double v_y = vol * Math.Sin(ang);
+                double x = 0, y = 0, t = 0.01;
+
+                in_File.WriteLine("0; {0};  {1}", x, y);
+
+                do
                 {
-                    in_File.Write(abscisa[i]);
-                    in_File.Write(';');
-                }
+                    x = x + (0.01 * v_x);
+                    y = y + (0.01 * v_y);
 
-                in_File.WriteLine(" ");
+                    if (y < 0)
+                        y = 0;
 
-                in_File.Write("y:");
-                in_File.Write(';');
-                for (int i = 0; i < time; i++)
-                {
-                    in_File.Write(ordinata[i]);
-                    in_File.Write(';');
+                    v_x = Vol_x(t, v_x, mass, 0.01);
+                    v_y = Vol_y(t, v_x, mass, 0.01);
+
+                    in_File.WriteLine("{0}; {1}; {2}", t, x, y);
+
+                    t += 0.01;
                 }
+                while (y > 0);
             }
+
         }
 
-        public void Write_Coordinates()
+        public double Wind_ver(double time)
         {
-            Console.WriteLine("t:   x   y");
-            for (int i = 0; i < time; i++)
-            {
-                Console.WriteLine("{0}:   {1}   {2}", i, abscisa[i], ordinata[i]);
-            }
+            return 9 * Math.Sin(5 * time);
         }
+        public double Wind_hor(double time)
+        {
+            return 7 * Math.Cos(2.5 * time);
+        }
+        public double Vol_x(double time, double vol, double mass, double delta)
+        {
+            return vol - (delta * Wind_hor(time) * vol / mass);
+        }
+        public double Vol_y(double time, double vol, double mass, double delta)
+        {
+            return vol - ((9.81 + (delta * Wind_ver(time) * vol)) / mass);
+        }
+
     }
+ 
     class Program
     {
         static void Main(string[] args)
         {
-            double angle, vol;
+            double angle, vol, mass;
             angle = Convert.ToDouble(Console.ReadLine());
             vol = Convert.ToDouble(Console.ReadLine());
-            
-            if(angle >= 90 || vol <= 0)
+            mass = Convert.ToDouble(Console.ReadLine());
+
+
+            if (angle >= 90 || vol <= 0 || mass <= 0)
             {
                 Console.WriteLine("Ошибка ввода данных, координаты полёта тела будут найденный неверно");
             }
 
-            Coordinates Hammer = new Coordinates(angle, vol);
+            Coordinates Hammer = new Coordinates(angle, vol, mass);
 
             Hammer.Write_to_File();
+            Hammer.Write_to_Console();
 
         }
     }
